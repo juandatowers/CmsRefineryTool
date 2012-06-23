5007d20c7cf64f2c1b4e65ac6ee39a0b39c7d002
System Administrator <root@TowersMacBook.(none)>
2012-06-22 21:52:26 -0500

    creacion proyecto cms en git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmstool::Application.config.secret_token = 'a6863d01b725e58075b2ebf1ca7cfd12944b332aacd10fb2fd5d0616a95d56aaf3fd7bbe9b814710fa59f45e210b2ea41e9ba04f5e496512cfd0fea1946a55f7'
